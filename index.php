cabba92a8b7435b2c9cbd27c40cd4bc4e2fc15fe
Chris Calascione <ccalascione@riversagency.com>
2018-05-08 11:14:14 -0400

    updates for grid category page with flexbox fallback

--- index.php
+++ index.php
@@ -8,14 +8,29 @@
         <link rel="stylesheet" href="build/style.css">
     </head>
     <body>
-        <div class="container">
-            <div class="sidebar">
-
+        <div class="custom-container">
+            <div class="breadcrumbs">
+                <img class="full-width img-responsive" src="assets/images/breadcrumbs.jpg" alt="breadcrumbs">
             </div>
-            <div class="product-grid">
-                <?php for ($i = 0; $i < 12; $i++): ?>
-                    <?php include('partials/product.php'); ?>
-                <?php endfor; ?>
+            <div class="category-grid">
+                <div class="sidebar">
+                    <img class="full-width img-responsive" src="assets/images/sidebar.jpg" alt="Sidebar">
+                </div>
+
+                <div class="right-column">
+                    <div class="header-image">
+                        <img class="full-width img-responsive" src="assets/images/category-header.jpg" alt="ring">
+                    </div>
+                    <div class="sorting">
+                        <img class="full-width img-responsive" src="assets/images/sort.jpg" alt="sorting">
+                    </div>
+                    <div class="product-grid">
+                        <?php for ($i = 0; $i < 12; $i++): ?>
+                            <?php include('partials/product.php'); ?>
+                        <?php endfor; ?>
+                    </div>
+                </div>
+
             </div>
 
         </div>
